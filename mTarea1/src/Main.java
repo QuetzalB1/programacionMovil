01bacfc1a2b33ae79ef8845017ebaf8bfc4e1ca9
Quetzal <157532374+QuetzalB1@users.noreply.github.com>
2025-10-25 14:22:17 -0600

    Delete mTarea1 directory
    delate

--- mTarea1/src/Main.java
+++
@@ -1,9 +0,0 @@
-// Alumno: Quetzalcoatl Yael Cruz Barron
-public class Main {
-    public static void main(String[] args) {
-        HolaMundo hm= new HolaMundo();
-        hm.nombre="omar";
-        hm.saludar("Ana");
-        hm.despedirse();
-    }
-}

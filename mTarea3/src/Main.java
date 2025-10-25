d880a716db3bfecab6b21a46d5ced00c4c159a3b
Quetzal <157532374+QuetzalB1@users.noreply.github.com>
2025-10-25 14:20:11 -0600

    Delete mTarea3 directory
    delate

--- mTarea3/src/Main.java
+++
@@ -1,8 +0,0 @@
-//Quetzalcoatl Yael Cruz Barron
-// Programacion Movil
-public class Main {
-    public static void main(String[] a) {
-        manejador m = new manejador();
-        m.setVisible(true);
-    }
-}
